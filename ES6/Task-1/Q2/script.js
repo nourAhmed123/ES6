cdd04b8201e544744ae15aacd461f9358e87a796
nourAhmed123 <96887154+nourAhmed123@users.noreply.github.com>
2022-08-11 12:32:39 +0200

    Description Added

--- ES6/Task-1/Q2/script.js
+++ ES6/Task-1/Q2/script.js
@@ -1,8 +1,13 @@
+alert(
+  "    Using rest parameter and spread operator return max value from any array note: array length is not fixed Bonus: return min and max value and display each of them separately after function call"
+);
+
 function MinMax() {
   var arr = [];
   arr = [1, 2, 3, 4, 5, 8, 7];
   var max = Math.max(...arr);
   var min = Math.min(...arr);
+  alert("The Max No. is " + max + "and the Min is  " + min);
   console.log(max);
   console.log(min);
 }
